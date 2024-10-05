1a7d76f86f3ebf30b3b478c0be26b9298f674e1e
MCVD <haroonrashidcc3@gmail.com>
2024-10-05 19:44:51 +0500

    Update synthetic_real_images_classification.py

--- views/projects/image_classification/synthetic_real_images_classification.py
+++ views/projects/image_classification/synthetic_real_images_classification.py
@@ -7,10 +7,10 @@ st.subheader("Project Overview", divider=True)
 st.write("""
 The 21st century has marked great advancements in the fields of technology, most notably in the field of AI. AI has 
 become an almost integral part of human life. Though AI has bore great fruits, still it has also resulted in the 
-spreading of a lot of mis-information, primarily in the form of fake images or videos which are produced using 
+spreading of a lot of misinformation, primarily in the form of fake images or videos which are produced using 
 generative AI.
 
-In this project I have trained a model that can accurately classify images to be either real or fake. **This project
+In this project, I have trained a model that can accurately classify images to be either real or fake. **This project
 has been implemented in both Keras and PyTorch**.
 """)
 
@@ -27,14 +27,13 @@ This dataset has two classes for classification, those being:
 1. Fake
 2. Real
 
-The training subset had a total of 100k images, 50k for each class, whereas the test subset has 20k images, 10k for each 
+The training subset had a total of 100k images, 50k for each class, whereas the test subset had 20k images, 10k for each 
 class.
 
-The dataset was already structured to have a train and test split. In this project I have used the test split for the
-purpose of validation. A batch size of 32 was used for training.
+The dataset was already structured to have a train and test split. In this project, I have used the test split for validation. A batch size of 32 was used for training.
 
 Before being fed to the model for training, both the training and validation splits were preprocessed to have a size of 
-32 pixels in width and height, rescaled to grayscale, and in case of the PyTorch implementation of this project, 
+32 pixels in width and height, rescaled to grayscale, and in the case of the PyTorch implementation of this project, 
 converted to torch tensors.
 """)
 
@@ -64,7 +63,7 @@ criterion for the PyTorch implementation). If the training loss did not decrease
 training would be terminated. As far as model checkpointing is concerned, after each epoch, the model with the best 
 training loss (lowest training loss) was saved.
 
-The trained model gave an accuracy higher than 96% (in case of PyTorch implementation the accuracy was calculated 
+The trained model gave an accuracy higher than 96% (in the case of PyTorch implementation the accuracy was calculated 
 using the **Accuracy class** provided in the **torchmetrics** package).
 """)
 
