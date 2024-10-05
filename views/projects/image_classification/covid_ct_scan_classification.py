571d3a1c4ee73155871354191184d8e8c0206424
MCVD <haroonrashidcc3@gmail.com>
2024-10-05 19:41:49 +0500

    Update covid_ct_scan_classification.py

--- views/projects/image_classification/covid_ct_scan_classification.py
+++ views/projects/image_classification/covid_ct_scan_classification.py
@@ -14,10 +14,10 @@ even worse was that it shared it's symptoms with common diseases like flu and ch
 became difficult for medical practitioners and the common person, to classify himself to be infected by COVID or not.
 
 Soon enough datasets which could be used to train AI models for the diagnosis of COVID started to surface on the
-internet, and were gratefully welcomed by AI engineers and researchers around the world.
+Internet, and were gratefully welcomed by AI engineers and researchers around the world.
 
-In this project I have trained a model for the accurate classification of the presence or non-presence of COVID, based 
-upon CT scans of the lungs of COVID infected patients and normal people. **This project has been implemented in both
+In this project, I have trained a model for the accurate classification of the presence or non-presence of COVID, based 
+on CT scans of the lungs of COVID infected patients and normal people. **This project has been implemented in both
 Keras and PyTorch**.
 """)
 
@@ -51,11 +51,11 @@ with st.container(height=300, border=True):
         )
 
 st.write("""
-This dataset was not structured to have a training and validation split. In case of this project, the entire dataset
+This dataset was not structured to have a training and validation split. In the case of this project, the entire dataset
 was used for training and no images were reserved for validation.
 
 Before being fed to the model for training, the training split was preprocessed to have a size of 128 pixels in
-width and height, rescaled to grayscale, and in case of the PyTorch implementation of this project, converted to
+width and height rescaled to grayscale, and in the case of the PyTorch implementation of this project, converted to
 torch tensors.
 """)
 
@@ -65,7 +65,7 @@ The architecture for this model can be found on my
 [GitHub](https://github.com/PythoneerSamurai/computer-vision-projects/tree/master/image-classification/sars-covid-two-ct-scan-classification).
 
 The model is a convolutional neural network, consisting of 3 2D-Convolutional layers, 3 2D-MaxPooling layers,
-3 2D-Dropout layers, 5 Rectified Linear Unit layers, and 3 linear layers, 3 2D-BatchNorm layers, and 1 sigmoid layer as 
+3 2D-Dropout layers, 5 Rectified Linear Unit layers, 3 linear layers, 3 2D-BatchNorm layers, and 1 sigmoid layer as 
 the final layer.
 """)
 
@@ -88,7 +88,7 @@ Only EarlyStopping was used as a callback for this project. The criterion for ea
 5 consecutive epochs (or the training accuracy did not increase for 5 consecutive epochs, in the case of PyTorch 
 implementation), the training would be terminated.
 
-The trained model gave an accuracy higher than 96% (in case of PyTorch implementation the accuracy was calculated 
+The trained model gave an accuracy higher than 96% (in the case of PyTorch implementation the accuracy was calculated 
 using the **Accuracy class** provided in the **torchmetrics** package).
 """)
 
