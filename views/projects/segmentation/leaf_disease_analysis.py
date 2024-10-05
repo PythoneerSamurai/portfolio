8388ca04ea9bbcf8c6673dca55c31335f1d38f9c
MCVD <haroonrashidcc3@gmail.com>
2024-10-05 19:03:43 +0500

    Update leaf_disease_analysis.py

--- views/projects/segmentation/leaf_disease_analysis.py
+++ views/projects/segmentation/leaf_disease_analysis.py
@@ -23,22 +23,22 @@ with st.container(height=380, border=True):
 st.write("\n")
 st.subheader("Project Overview", divider=True)
 st.write("""
-In this project I have utilized semantic segmentation for the purpose of segmenting leaf diseases. I have successfully
-trained a model which can perform the aforementioned task with high precision and accuracy.
+In this project, I have utilized semantic segmentation to segment leaf diseases. I have successfully
+trained a model that can perform the task mentioned earlier with high precision and accuracy.
 """)
 
 st.write("\n")
 st.subheader("Training Pipeline", divider=True)
 st.write("##### Training Data")
 st.write("""
-In the case of this project, only one yolov8x model has been utilized, that for the purpose of leaf disease 
+In the case of this project, only one yolov8x model has been utilized, for leaf disease 
 segmentation.
 
 The dataset used to train the model is the
-[Leaf disease segmentation dataset](https://www.kaggle.com/datasets/fakhrealam9537/leaf-disease-segmentation-dataset) 
-dataset provided by "**Fakhre Alam**" on Kaggle.
+[Leaf disease segmentation dataset](https://www.kaggle.com/datasets/fakhrealam9537/leaf-disease-segmentation-dataset)
+provided by "**Fakhre Alam**" on Kaggle.
 
-The dataset has only one class for segmentation, that is "disease".
+The dataset has only one segmentation class, "disease".
 
 The datasets were preprocessed and the images had a size of 640 pixels in width and height. The datasets did not have
 YOLO format .txt segmentation labels, rather they came with binary masks, therefore I used an open-source script to
@@ -50,11 +50,11 @@ had sub-directories "train", "test", and "val".
 
 st.write("##### Model Used")
 st.write("""
-As mentioned above, only one model has been used in this project, that is a **yolov8x** model for leaf disease
+As mentioned above, only one model has been used in this project: a **yolov8x** model for leaf disease
 segmentation. The model is the largest variant of its kind (as inferred by the "x" in the name of the model), and thus 
 has the highest mAP (Mean Average Precision) value, as benchmarked on the COCO dataset.
 
-In the case of this project, pretrained model (yolov8x.pt) was not used, rather a new model (yolov8x.yaml) was trained 
+In the case of this project, a pre-trained model (yolov8x.pt) was not used, rather a new model (yolov8x.yaml) was trained 
 from scratch (this resulted in better precision and recall).
 
 The model was accessed and trained using the Ultralytics YOLO API, as provided in the **"ultralytics"** Python
@@ -66,15 +66,15 @@ For more information regarding the YOLO models, refer to the
 
 st.write("##### Training Analysis")
 st.write("""
-The model was trained using the ".train" function present in the ultralytics YOLO class.
+The model was trained using the ".train" function in the ultralytics YOLO class.
 
-The maximum amount of epochs that the model could train for was set to 600, with a patience of 50 epochs
+The maximum amount of epochs that the model could train for was set to 600, with the patience of 50 epochs
 (patience is a hyper-parameter that defines how many epochs will pass before EarlyStopping stops the training due to no
-improvement). The model was trained using Kaggle cloud computing and thus two Nvidia Tesla T4 GPUs were used for
+improvement). The model was trained using Kaggle cloud computing; thus, two Nvidia Tesla T4 GPUs were used for
 training the model. The optimizer was set to "auto", due to which "Stochastic Gradient Descent (aka SGD)" was used as 
 the optimizer. Default values were used for all other hyper-parameters.
 
-The model was trained for several hours before EarlyStopping terminated the trainings.
+The model was trained for several hours before EarlyStopping terminated the training.
 """)
 
 st.write("##### Model Links")
@@ -128,7 +128,7 @@ The input image is also loaded into memory using the cv2.imread() function.
 st.write("##### Getting Model's Inference and Defining the Frame Processor Function")
 st.write("""
 Lastly in the prediction pipeline, the trained model's inference is carried out on the input image. Later on, a function
-is defined for the purpose of handling all functionality, ranging from inference conversion (from YOLO format to
+is defined to handle all functionality, ranging from inference conversion (from YOLO format to
 supervision format) to the saving of the annotated image.
 """)
 
