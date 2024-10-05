fa82fd701bf1e77eba3f1debc094ec5cfd092c17
MCVD <haroonrashidcc3@gmail.com>
2024-10-05 19:44:04 +0500

    Update pneumonia_classification.py

--- views/projects/image_classification/pneumonia_classification.py
+++ views/projects/image_classification/pneumonia_classification.py
@@ -5,7 +5,7 @@ st.title("Pneumonia Classification")
 st.write("\n")
 st.subheader("Project Overview", divider=True)
 st.write("""
-In this project I have trained a model for classifying x-ray images of lungs in order to identify three types of
+In this project, I have trained a model for classifying X-ray images of lungs to identify three types of
 pneumonia, those being viral, bacterial, and COVID-19 variant. **This project has been implemented in both Keras and
 PyTorch**.
 """)
@@ -57,7 +57,7 @@ Since the dataset was not structured to have a validation and training split, th
 used for training and 20% were used for validation. A batch size of 32 was used for training.
 
 Before being fed to the model for training, the training split was preprocessed to have a size of 128 pixels in
-width and height, rescaled to grayscale, and in case of the PyTorch implementation of this project, converted to
+width and height rescaled to grayscale, and in the case of the PyTorch implementation of this project, converted to
 torch tensors, and then assigned the data type torch.float32.
 """)
 
@@ -81,7 +81,7 @@ with st.container(height=400, border=True):
 st.write("#### Training Analysis")
 st.write("""
 The model was trained for at most 30 epochs, with **Sparse Categorical CrossEntropy** being used as the loss
-function in the keras implementation and **CrossEntropy** being used as the loss function in the PyTorch
+function in the Keras implementation and **CrossEntropy** being used as the loss function in the PyTorch
 implementation (both losses are essentially the same). Adam was used as the optimizer, with default 
 hyper-parameter values, in both implementations.
 
@@ -90,7 +90,7 @@ and ModelCheckpoint. The criterion for early stopping was set to training loss. 
 decrease for 5 consecutive epochs, the training would be terminated. As far as model checkpointing is concerned, 
 after each epoch, the model with the best training loss (lowest training loss) was saved.
 
-The trained model gave an accuracy higher than 96% (in case of PyTorch implementation the accuracy was calculated 
+The trained model gave an accuracy higher than 96% (in the case of PyTorch implementation the accuracy was calculated 
 using the **Accuracy class** provided in the **torchmetrics** package).
 """)
 
