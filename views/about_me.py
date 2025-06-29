83dfd59aa4a24f4edda18951c6173b7dabc4d0ea
MCVD <haroonrashidcc3@gmail.com>
2025-06-29 10:40:47 +0500

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -25,7 +25,7 @@ with col2:
             label="Download Resume",
             data=PDFByte,
             file_name="resume.pdf",
-            mime="application/octet-stream",
+            mime="application/pdf",
         )
     with subCol2:
         if st.button("Links"):
