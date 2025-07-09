fd3a0cc21f1b0c2fcf71adfa119cd371a215952f
MCVD <haroonrashidcc3@gmail.com>
2025-07-09 11:20:20 +0500

    Update dwell_time_analysis.py

--- views/projects/object detection/dwell_time_analysis.py
+++ views/projects/object detection/dwell_time_analysis.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Dwell Time Analysis")
 st.write("\n")
 
-with st.container(height=400, border=True):
+with st.container(border=True):
     st.video("assets/object_detection/dwell_time_analysis/input_video.mp4", muted=True)
 
 st.write("\n")
